e064edb8294d661ee42b92b6f0e55e0a96aa7b85
mohitech25 <mohitech25@gmail.com>
2025-09-17 14:21:53 +0600

    Update PayServiceProvider: load migrations, bind Payment model, add optional auto-publish

--- src/PayServiceProvider.php
+++ src/PayServiceProvider.php
@@ -3,13 +3,28 @@
 namespace MoHiTech\MoHiPay;
 
 use Illuminate\Support\ServiceProvider;
+use MoHiTech\MoHiPay\Models\Payment;
 
 class PayServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../Models/Payment.php' => app_path('Models/Payment.php'),
+            ], 'mohipay-models');
+
+            $this->callAfterResolving(\Illuminate\Foundation\Application::class, function () {
+                $this->app->make(\Illuminate\Contracts\Console\Kernel::class)->call('vendor:publish', [
+                    '--tag' => 'mohipay-models',
+                    '--force' => true,
+                ]);
+            });
+        }
     }
+
 
     public function register()
     {
